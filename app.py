8c236b6e6fcd5bf68e0320b38dcd7d2c245c223d
CWright2022 <61096126+CWright2022@users.noreply.github.com>
2023-03-06 20:25:44 -0500

    logging working

--- app.py
+++ app.py
@@ -19,17 +19,19 @@ OUTPUT_TEXT = ""
 @app.route('/', methods=["GET", "POST"])
 def handle():
     global OUTPUT_TEXT
-    logging.info("new connection from: {0}").format(request.environ.get("HTTP_X_REAL_IP", request.remote_addr))
-    logging.info("user agent: {0}".format(request.remote_user))
     if request.method == "GET":
+        logging.info("new connection from: {0}".format(request.environ.get("HTTP_X_REAL_IP", request.remote_addr)))
+        logging.info("user agent: {0}".format(request.headers.get("User-Agent")))
         return render_template("index.html")
     elif request.method == "POST":
         text = request.form.get("text")
         operation = request.form.get("chooser")
         if operation == "decrypt":
             OUTPUT_TEXT = decrypt(text, "./script.txt")
+            logging.info("user decrypted to the following text: {0}".format(OUTPUT_TEXT))
         else:
             OUTPUT_TEXT = encrypt(text, "./script.txt")
+            logging.info("user encrypted the following text: {0}".format(text))
         return redirect(
             url_for('success'))
 
