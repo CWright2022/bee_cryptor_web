1e3af783844392864d974c3f37a26a2176cd4e41
CWright2022 <61096126+CWright2022@users.noreply.github.com>
2023-11-16 13:26:00 -0500

    fix broken links

--- app.py
+++ app.py
@@ -20,15 +20,18 @@ OUTPUT_TEXT = ""
 def handle():
     global OUTPUT_TEXT
     if request.method == "GET":
-        logging.info("new connection from: {0}".format(request.environ.get("HTTP_X_REAL_IP", request.remote_addr)))
-        logging.info("user agent: {0}".format(request.headers.get("User-Agent")))
+        logging.info("new connection from: {0}".format(
+            request.environ.get("HTTP_X_REAL_IP", request.remote_addr)))
+        logging.info("user agent: {0}".format(
+            request.headers.get("User-Agent")))
         return render_template("index.html")
     elif request.method == "POST":
         text = request.form.get("text")
         operation = request.form.get("chooser")
         if operation == "decrypt":
             OUTPUT_TEXT = decrypt(text, "./script.txt")
-            logging.info("user decrypted to the following text: {0}".format(OUTPUT_TEXT))
+            logging.info(
+                "user decrypted to the following text: {0}".format(OUTPUT_TEXT))
         else:
             OUTPUT_TEXT = encrypt(text, "./script.txt")
             logging.info("user encrypted the following text: {0}".format(text))
